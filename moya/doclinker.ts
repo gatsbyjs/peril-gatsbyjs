6a53bfd8c622b204f45dcfa6e01fa057f1bb1e0e
Steven Deutsch <stevensdeutsch@yahoo.com>
2018-03-28 09:36:47 -0500

    Make code review changes

--- moya/doclinker.ts
+++ moya/doclinker.ts
@@ -8,61 +8,61 @@ var linkToKeywordMap = [
     keywords: new Set(["target", "targettype"])
   },
   { link: "[Providers](https://github.com/Moya/Moya/blob/master/docs/Providers.md)", 
-  keywords: new Set(["provider", "providers", "moyaprovider", "rxmoyaprovider", "reactivemoyaprovider"])
+    keywords: new Set(["provider", "providers", "moyaprovider", "rxmoyaprovider", "reactivemoyaprovider"])
   },
   { link: "[Endpoints](https://github.com/Moya/Moya/blob/master/docs/Endpoints.md)", 
-  keywords: new Set(["endpoint", "endpoints", "endpointclosure", "requestclosure"])
+    keywords: new Set(["endpoint", "endpoints", "endpointclosure", "requestclosure"])
   },
   { link: "[MultiTarget](https://github.com/Moya/Moya/blob/master/docs/Examples/MultiTarget.md)", 
-  keywords: new Set(["multitarget"])
+    keywords: new Set(["multitarget"])
   },
   { link: "[MultipartUpload](https://github.com/Moya/Moya/blob/master/docs/Examples/MultipartUpload.md)", 
-  keywords: new Set(["multipart"])
+    keywords: new Set(["multipart"])
   },
   { link: "[ParameterEncoding](https://github.com/Moya/Moya/blob/master/docs/Examples/ParameterEncoding.md)", 
-  keywords: new Set(["encoding"])
+    keywords: new Set(["encoding"])
   },
   { link: "[Response](https://github.com/Moya/Moya/blob/master/docs/Examples/Response.md)", 
-  keywords: new Set(["response", "moya.response"])
+    keywords: new Set(["response", "moya.response"])
   },
   { link: "[ErrorTypes](https://github.com/Moya/Moya/blob/master/docs/Examples/ErrorTypes.md)", 
-  keywords: new Set(["moyaerror"])
+    keywords: new Set(["moyaerror"])
   },
   { link: "[RxSwift](https://github.com/Moya/Moya/blob/master/docs/RxSwift.md)", 
-  keywords: new Set(["rxswift, rxmoya, rx"])
+    keywords: new Set(["rxswift, rxmoya, rx"])
   },
   { link: "[ReactiveSwift](https://github.com/Moya/Moya/blob/master/docs/ReactiveSwift.md)", 
-  keywords: new Set(["reactiveswift", "reactivemoya"])
+    keywords: new Set(["reactiveswift", "reactivemoya"])
   },
   { link: "[Threading](https://github.com/Moya/Moya/blob/master/docs/Threading.md)", 
-  keywords: new Set(["thread", "threading"])
+    keywords: new Set(["thread", "threading"])
   },
   { link: "[Authentication](https://github.com/Moya/Moya/blob/master/docs/Authentication.md)", 
-  keywords: new Set(["auth", "authentication", "oauth"])
+    keywords: new Set(["auth", "authentication", "oauth"])
   },
   { link: "[Plugins](https://github.com/Moya/Moya/blob/master/docs/Plugins.md)", 
-  keywords: new Set(["plugin", "plugins", "plugintype"])
+    keywords: new Set(["plugin", "plugins", "plugintype"])
   },
   { link: "[AuthPlugin](https://github.com/Moya/Moya/blob/master/docs/Examples/AuthPlugin.md)", 
   keywords: new Set(["authplugin", "auth", "plugin", "plugintype"])
   },
   { link: "[Custom Plugin](https://github.com/Moya/Moya/blob/master/docs/Examples/CustomPlugin.md)", 
-  keywords: new Set(["plugin", "plugintype"])
+    keywords: new Set(["plugin", "plugintype"])
   },
   { link: "[Alamofire Validation](https://github.com/Moya/Moya/blob/master/docs/Examples/AlamofireValidation.md)", 
-  keywords: new Set(["alamofirevalidation", "validation", "validationtype", "validate"])
+    keywords: new Set(["alamofirevalidation", "validation", "validationtype", "validate"])
   }
 ]
 
 // Variables / Constants
 
-const gh = danger.github as any;
-const issue = gh.issue;
+const gh = danger.github as any
+const issue = gh.issue
 const username = issue.user.login
-const repo = gh.repository;
+const repo = gh.repository
 
 const title = issue.title.toLowerCase()
-const titleWords = title.split(" ")
+const titleWords = title.split(" ") as string[]
 
 const commentHeader = `
   Hey @${username},
@@ -81,21 +81,12 @@ const commentFooter = `
 // Functions
 
 const isIssueQuestion = (): boolean => {
-  const labels = issue.labels
-  for (var i = 0; i < labels.length; i++) {
-    const label = labels[i]
-    if (label.name == "question") { return true }
-  }
-  return false
+  const labels = danger.github.issue.labels
+  return labels.map(i => i.name).includes('question')
 }
 
 const titleIncludesAny = (words: Set<string>): boolean => {
-  if (titleWords.length == 0) { return false }
-  for (var i = 0; i < titleWords.length; i++) {
-    const titleWord = titleWords[i]
-    if (words.has(titleWord)) { return true }
-  }
-  return false
+  return titleWords.filter(titleWord => words.has(titleWord)).length > 0
 }
 
 const createCommentBody = () => {

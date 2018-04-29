817ed685ffa971d7245384db7bc9a14c4474af23
Steven Deutsch <stevensdeutsch@yahoo.com>
2018-04-29 06:26:18 -0500

    Export labeler

--- org/labeler.ts
+++ org/labeler.ts
@@ -1,111 +1,156 @@
-import { danger, schedule } from "danger"
+import { danger, schedule } from "danger";
 
-const gh = danger.github as any
-const issue = gh.issue
-const repo = gh.repository
+// The inspiration for this is https://github.com/artsy/artsy-danger/blob/f019ee1a3abffabad65014afabe07cb9a12274e7/org/all-prs.ts
+const isJest = typeof jest !== "undefined";
+// Returns the promise itself, for testing.
+const _test = (reason: string, promise: Promise<any>) => promise;
+// Schedules the promise for execution via Danger.
+const _run = (reason: string, promise: Promise<any>) => schedule(promise);
+const wrap: any = isJest ? _test : _run;
 
-const title = issue.title.toLowerCase()
-const titleWords = title.split(" ") as string[]
+export const labeler = wrap(
+  "Label newly created issue based on title keywords",
+  async () => {
+    const gh = danger.github as any;
+    const repo = gh.repository;
+    const issue = gh.issue;
 
-let labelsToAdd: string[] = []
+    const title = issue.title.toLowerCase();
+    const titleWords = title.split(" ") as string[];
 
-// Helpers
+    let labelsToAdd: string[] = [];
 
-const titleIncludesAny = (words: Set<string>): boolean => {
-  return titleWords.filter(titleWord => words.has(titleWord)).length > 0
-}
+    // Helpers
 
-const titleStartsWithAny = (words: Set<string>): boolean => {
-  if (titleWords.length == 0) { return false }
-  const firstWord = titleWords[0]
-  return words.has(firstWord)
-}
+    const titleIncludesAny = (words: Set<string>): boolean => {
+      return titleWords.filter(titleWord => words.has(titleWord)).length > 0;
+    };
 
-const titleEndsInQuestionMark = (): boolean => {
-  return title.slice(-1) == "?"
-}
+    const titleStartsWithAny = (words: Set<string>): boolean => {
+      if (titleWords.length == 0) {
+        return false;
+      }
+      const firstWord = titleWords[0];
+      return words.has(firstWord);
+    };
 
-const addLabels = (names: string[]) => {
-  schedule(async () => {
-    await danger.github.api.issues.addLabels({
-      owner: repo.owner.login,
-      repo: repo.name,
-      number: issue.number,
-      labels: names
-    })
-  })
-}
+    const titleEndsInQuestionMark = (): boolean => {
+      return title.slice(-1) == "?";
+    };
 
-// label: question
+    // label: question
 
-const questionWords: Set<string> = new Set(["how", "who", "what", "where", "when", "why", "which"])
+    const questionWords: Set<string> = new Set([
+      "how",
+      "who",
+      "what",
+      "where",
+      "when",
+      "why",
+      "which"
+    ]);
 
-if (titleEndsInQuestionMark() || titleStartsWithAny(questionWords)) {
-  labelsToAdd.push("question")
-}
+    if (titleEndsInQuestionMark() || titleStartsWithAny(questionWords)) {
+      labelsToAdd.push("question");
+    }
 
-// label: documentation
+    // label: documentation
 
-const documentationWords: Set<string> = new Set(["documentation", "document", "docs", "doc", "readme", "changelog"])
+    const documentationWords: Set<string> = new Set([
+      "documentation",
+      "document",
+      "docs",
+      "doc",
+      "readme",
+      "changelog"
+    ]);
 
-if (titleIncludesAny(documentationWords)) {
-  labelsToAdd.push("documentation")
-}
+    if (titleIncludesAny(documentationWords)) {
+      labelsToAdd.push("documentation");
+    }
 
-// label: enhancement
+    // label: enhancement
 
-const enhancementWords: Set<string> = new Set(["add", "allow", "improve", "upgrade", "update", "support"])
+    const enhancementWords: Set<string> = new Set([
+      "add",
+      "allow",
+      "improve",
+      "upgrade",
+      "update",
+      "support"
+    ]);
 
-if (titleStartsWithAny(enhancementWords)) {
-  labelsToAdd.push("enhancement")
-}
+    if (titleStartsWithAny(enhancementWords)) {
+      labelsToAdd.push("enhancement");
+    }
 
-// label: cocoapods
+    // label: cocoapods
 
-const cocoaPodsWords: Set<string> = new Set(["pod", "cocoapod", "cocoapods"])
+    const cocoaPodsWords: Set<string> = new Set([
+      "pod",
+      "cocoapod",
+      "cocoapods"
+    ]);
 
-if (titleIncludesAny(cocoaPodsWords)) {
-  labelsToAdd.push("cocoapods")
-}
+    if (titleIncludesAny(cocoaPodsWords)) {
+      labelsToAdd.push("cocoapods");
+    }
 
-// label: carthage
+    // label: carthage
 
-const carthageWords: Set<string> = new Set(["carthage", "cartfile"])
+    const carthageWords: Set<string> = new Set(["carthage", "cartfile"]);
 
-if (titleIncludesAny(carthageWords)) {
-  labelsToAdd.push("carthage")
-}
+    if (titleIncludesAny(carthageWords)) {
+      labelsToAdd.push("carthage");
+    }
 
-// label: bug?
+    // label: bug?
 
-const bugWords: Set<string> = new Set(["bug", "crash", "leak"])
+    const bugWords: Set<string> = new Set(["bug", "crash", "leak"]);
 
-if (titleIncludesAny(bugWords)) {
-  labelsToAdd.push("bug?")
-}
+    if (titleIncludesAny(bugWords)) {
+      labelsToAdd.push("bug?");
+    }
 
-// label: rxmoya
+    // label: rxmoya
 
-const rxWords: Set<string> = new Set(["rxmoya", "rxswift", "rx"])
+    const rxWords: Set<string> = new Set(["rxmoya", "rxswift", "rx"]);
 
-if (titleIncludesAny(rxWords)) {
-  labelsToAdd.push("rxmoya") 
-}
+    if (titleIncludesAny(rxWords)) {
+      labelsToAdd.push("rxmoya");
+    }
 
-// label: reactivemoya
+    // label: reactivemoya
 
-const reactiveWords: Set<string> = new Set(["reactivemoya", "reactiveswift", "rac"])
+    const reactiveWords: Set<string> = new Set([
+      "reactivemoya",
+      "reactiveswift",
+      "rac"
+    ]);
 
-if (titleIncludesAny(reactiveWords)) {
-  labelsToAdd.push("reactivemoya")
-}
+    if (titleIncludesAny(reactiveWords)) {
+      labelsToAdd.push("reactivemoya");
+    }
 
-// label: spm
+    // label: spm
 
-const spmWords: Set<string> = new Set(["spm", "package.swift", "package.resolved"])
+    const spmWords: Set<string> = new Set([
+      "spm",
+      "package.swift",
+      "package.resolved"
+    ]);
 
-if (titleIncludesAny(spmWords)) {
-  labelsToAdd.push("spm")
-}
+    if (titleIncludesAny(spmWords)) {
+      labelsToAdd.push("spm");
+    }
 
-addLabels(labelsToAdd)
+    if (labelsToAdd.length > 0) {
+      await danger.github.api.issues.addLabels({
+        owner: repo.owner.login,
+        repo: repo.name,
+        number: issue.number,
+        labels: labelsToAdd
+      });
+    }
+  }
+);

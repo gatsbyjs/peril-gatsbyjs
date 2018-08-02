a64222fdf1f5af641a9027da5878490be4d5794e
Piotr Kwiecinski <piokwiecinski@gmail.com>
2018-08-02 22:16:28 +0100

    Add automated labeling for questions and documentation based on title

---
+++ org/labeler.ts
@@ -0,0 +1,86 @@
+import { danger, schedule } from 'danger';
+
+// The inspiration for this is https://github.com/artsy/artsy-danger/blob/f019ee1a3abffabad65014afabe07cb9a12274e7/org/all-prs.ts
+const isJest = typeof jest !== 'undefined';
+// Returns the promise itself, for testing.
+const _test = (reason: string, promise: Promise<any>) => promise;
+// Schedules the promise for execution via Danger.
+const _run = (reason: string, promise: Promise<any>) => schedule(promise);
+const wrap: any = isJest ? _test : _run;
+
+export const labeler = wrap(
+  'Label newly created issue based on keywords',
+  async () => {
+    const gh = danger.github as any;
+    const repo = gh.repository;
+    const issue = gh.issue;
+    const title = issue.title;
+    const titleWords = title.split(" ") as string[];
+
+    let labelsToAdd: string[] = [];
+
+    const titleIncludesAny = (words: Set<string>): boolean => {
+      return titleWords.filter(titleWord => words.has(titleWord.toLowerCase())).length > 0;
+    };
+
+    const titleStartsWithAny = (words: Set<string>): boolean => {
+      if (titleWords.length == 0) {
+        return false;
+      }
+
+      const firstWord = titleWords[0];
+      return words.has(firstWord.toLowerCase());
+    };
+
+      const titleEndsInQuestionMark = (): boolean => {
+          return title.slice(-1) == "?";
+      };
+
+    const addLabelIfDoesNotExist = (name: string) => {
+      const labels = danger.github.issue.labels;
+      const hasLabel = labels.map(i => i.name).includes(name);
+      if (!hasLabel) {
+        labelsToAdd.push(name);
+      }
+    };
+
+    // type: question or discussion
+    const questionWords: Set<string> = new Set([
+      'how',
+      'who',
+      'what',
+      'where',
+      'when',
+      'why',
+      'which',
+    ]);
+
+    if (titleEndsInQuestionMark() || titleStartsWithAny(questionWords)) {
+      addLabelIfDoesNotExist('type: question or discussion');
+    }
+
+    // label: documentation
+    const documentationWords: Set<string> = new Set([
+      'documentation',
+      'document',
+      'docs',
+      'doc',
+      'readme',
+      'changelog',
+      'tutorial',
+    ]);
+
+    if (titleIncludesAny(documentationWords)) {
+      addLabelIfDoesNotExist('type: documentation');
+    }
+
+    if (labelsToAdd.length > 0) {
+      await danger.github.api.issues.addLabels({
+        owner: repo.owner.login,
+        repo: repo.name,
+        number: issue.number,
+        labels: labelsToAdd
+      });
+    }
+  }
+);
---
+++ tests/labeler.test.ts
@@ -0,0 +1,84 @@
+jest.mock('danger', () => jest.fn());
+import * as danger from 'danger';
+const dm = danger as any;
+
+import { labeler } from '../org/labeler';
+
+beforeEach(() => {
+  dm.danger = {
+    github: {
+      repository: {
+        name: 'gatsby',
+        owner: {
+          login: "gatsbyjs"
+        }
+      },
+      issue: {
+        labels: [],
+        number: 100
+      },
+      api: {
+        issues: {
+          addLabels: jest.fn()
+        }
+      }
+    }
+  };
+});
+
+describe('a new issue', () => {
+  it('with question mark in a title', () => {
+    dm.danger.github.issue.title = 'Help - Has anyone hosted a gatsby.js site on Platform.sh?';
+    return labeler().then(() => {
+      expect(dm.danger.github.api.issues.addLabels).toBeCalledWith({
+        repo: 'gatsby',
+        owner: 'gatsbyjs',
+        number: 100,
+        labels: ['type: question or discussion']
+      })
+    });
+  });
+
+  it('starting with how', () => {
+    dm.danger.github.issue.title = 'How do you justify Gatsby’s bundle size to clients';
+    return labeler().then(() => {
+      expect(dm.danger.github.api.issues.addLabels).toBeCalledWith({
+        repo: 'gatsby',
+        owner: 'gatsbyjs',
+        number: 100,
+        labels: ['type: question or discussion']
+      })
+    });
+  });
+
+  it('including tutorial', () => {
+    dm.danger.github.issue.title = 'Tutorial template + gold standard example';
+    return labeler().then(() => {
+      expect(dm.danger.github.api.issues.addLabels).toBeCalledWith({
+        repo: 'gatsby',
+        owner: 'gatsbyjs',
+        number: 100,
+        labels: ['type: documentation']
+      })
+    });
+  });
+
+  it('including readme', () => {
+    dm.danger.github.issue.title = '[v2] default starter: update README';
+    return labeler().then(() => {
+      expect(dm.danger.github.api.issues.addLabels).toBeCalledWith({
+        repo: 'gatsby',
+        owner: 'gatsbyjs',
+        number: 100,
+        labels: ['type: documentation']
+      })
+    });
+  });
+
+  it('not recognised', () => {
+    dm.danger.github.issue.title = 'Supporting HSTS and HSTS preloading';
+    return labeler().then(() => {
+      expect(dm.danger.github.api.issues.addLabels).not.toBeCalled()
+    })
+  });
+});

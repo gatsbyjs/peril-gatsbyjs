b359dae20cfd44f87d1e8a48c8af52d0f78cb62d
Jason Lengstorf <jason@lengstorf.com>
2018-08-17 16:49:01 -0700

    temp: test testable-scheduler

--- rules/labeler.ts
+++ rules/labeler.ts
@@ -1,13 +1,10 @@
 import { schedule } from 'danger';
-// import schedule from '../utils/testable-schedule';
-
-const isJest = typeof jest !== 'undefined';
 
 // The inspiration for this is https://git.io/fNh6i
 // If `jest` is defined, we’re testing, so just return the Promise.
 // If `jest` is undefined, actually schedule for execution with Danger.
 const testableSchedule = (reason: string, action: any) =>
-  isJest ? action : schedule(action);
+  typeof jest !== 'undefined' ? action : schedule(action);
 
 export const labeler = testableSchedule(
   'Label newly created issue based on keywords',

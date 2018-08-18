920eebbdc0c063c040e972ec535c2b632b66e066
Jason Lengstorf <jason@lengstorf.com>
2018-08-17 18:31:38 -0700

    feat: why does it run multiple times?

--- rules/emptybody.ts
+++ rules/emptybody.ts
@@ -1,28 +1,39 @@
-import { danger, schedule, markdown } from 'danger';
+import { danger, schedule } from 'danger';
 
 // Make schedule testable with Jest. Inspiration: https://git.io/fNh6i
 const testableSchedule = (reason: string, action: any) =>
   typeof jest !== 'undefined' ? action : schedule(action);
 
-console.log('emptybody.ts was loaded');
+let count = 0;
 
 export const emptybody = testableSchedule(
   'Request more information if issue body is empty',
   async () => {
-    console.log('emptybody.ts was executed');
     const gh = danger.github as any;
     const issue = gh.issue;
     const repo = gh.repository;
     const body = issue.body.trim();
     const username = issue.user.login;
 
-    if (body.length == 0) {
-      const comment = `
-  @${username} We noticed that the body of this issue is blank.
+    console.log(`Empty body ran ${count++} times.`);
+    if (count > 1) {
+      // Only post once!
+      return;
+    }
 
-  Please fill in this field with more information to help the maintainers resolve your issue.
-`;
-      markdown(comment);
+    const comment = `
+@${username} We noticed that the body of this issue is blank.
+
+Please fill in this field with more information to help the maintainers resolve your issue.
+    `;
+
+    if (body.length == 0) {
+      await danger.github.api.issues.createComment({
+        owner: repo.owner.login,
+        repo: repo.name,
+        number: issue.number,
+        body: comment
+      });
     }
   }
 );

a54dda52509deebb02cc94d5a9a5575b67748edb
Jason Lengstorf <jason@lengstorf.com>
2018-08-17 17:57:28 -0700

    temp: add logging

--- rules/emptybody.ts
+++ rules/emptybody.ts
@@ -4,9 +4,12 @@ import { danger, schedule, markdown } from 'danger';
 const testableSchedule = (reason: string, action: any) =>
   typeof jest !== 'undefined' ? action : schedule(action);
 
+console.log('emptybody.ts was loaded');
+
 export const emptybody = testableSchedule(
   'Request more information if issue body is empty',
   async () => {
+    console.log('emptybody.ts was executed');
     const gh = danger.github as any;
     const issue = gh.issue;
     const repo = gh.repository;

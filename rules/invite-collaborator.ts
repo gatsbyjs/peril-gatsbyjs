81fe310dd743bff398536a8ddaddcae7e8e7d74c
Yugandhar Tripathi <tripathi.yugandhar@gmail.com>
2018-10-05 06:43:34 +0530

    feat: added invite link (#44)
    closes #42

--- rules/invite-collaborator.ts
+++ rules/invite-collaborator.ts
@@ -6,7 +6,7 @@ Holy buckets, @${username} — we just merged your PR to Gatsby! 💪💜
 Gatsby is built by awesome people like you. Let us say “thanks” in two ways:
 
  1.  **We’d like to send you some Gatsby swag.** As a token of our appreciation, you can go to the [Gatsby Swag Store][store] and log in with your GitHub account to get a coupon code good for one free piece of swag. (Currently we’ve got a couple t-shirts available, plus some socks that are really razzing our berries right now.)
- 2.  **We just invited you to join the Gatsby organization on GitHub.** This will add you to our team of maintainers. You’ll receive an email shortly asking you to confirm. By joining the team, you’ll be able to label issues, review pull requests, and merge approved pull requests.
+ 2.  **We just invited you to join the Gatsby organization on GitHub.** This will add you to our team of maintainers. Accept the invite by visiting https://github.com/orgs/gatsbyjs/invitation . By joining the team, you’ll be able to label issues, review pull requests, and merge approved pull requests.
 
 If there’s anything we can do to help, please don’t hesitate to reach out to us: tweet at [@gatsbyjs][twitter] and we’ll come a-runnin’.
 
